d0eaf9ef88b07129f2490876d482c5f25316cd25
Adarshused <adarshmishr6@gmail.com>
2025-09-16 15:50:01 +0530

    initialize project structure with docker-compose, go modules, and GraphQL setup

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Adarshused/Ecommerce-MicroService-GO
+
+go 1.25.0
+
+require github.com/99designs/gqlgen v0.17.79
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.30 // indirect
+)
---
+++ graphQL/graph.go
@@ -0,0 +1,57 @@
+package main
+
+
+
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/Adarshused/Ecommerce-Microservice-GO/account"
+	"github.com/Adarshused/Ecommerce-Microservice-GO/catalog"
+	"github.com/Adarshused/Ecommerce-Microservice-GO/order"
+)
+
+type Server struct {
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	orderClient *order.Client
+} 
+
+
+
+func NewGraphQLServer(accountURL, catalogURL, orderURL string) (*Server, error) {
+
+	// connect to account service
+	accountClient, err := account.NewClient(accountURL)
+
+	if err != nil {
+		return nil, err;
+	}
+
+	// connect to catalog servie
+	catalogClient, err := catalog.NewClient(catalogURL)
+
+	if err != nil {
+		return nil, err;
+	}
+
+	// connect to order service
+	orderClient, err := order.NewClient(orderURL)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Server{
+		accountClient,
+		catalogClient,
+		orderClient,
+	}, nil
+
+}
+
+
+// func (s *Server) Mutation() MutationResolver {
+
+//        return &mutationResolver{
+// 		server: s,
+// 	   }
+// }
